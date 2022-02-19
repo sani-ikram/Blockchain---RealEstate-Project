4e76ed261bce6d20572162b1aeb8e7f334874aaf
sani-ikram <78637579+sani-ikram@users.noreply.github.com>
2022-02-19 06:24:38 +0000

    Add files via upload

--- mint.js
+++ mint.js
@@ -2,29 +2,17 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const web3 = require("web3");
 const isInfura="5f8ee2028e694536993d5409ed4a8319";
 const privatekeys = ['40186f69e3ca20183b76d676520be3b829f392b9a2ae9e776d923c59851340b2'];
-//const NODE_API_KEY = "2JAiypNdm1qMbPUG1bSujLPrFZESmc3M";
+const fetch = require("node-fetch");
+
 const NODE_API_KEY = `https://eth-rinkeby.alchemyapi.io/v2/2JAiypNdm1qMbPUG1bSujLPrFZESmc3M`;
 const  MNEMONIC="magic lake smooth entry reduce candy bleak tray text spawn tuition hill";
 const  OWNER_ADDRESS="0xA13C2656c1403c4B4a82421C775E93f29c493B81";
-//const NFT_CONTRACT_ADDRESS="0xD7e275c28463dD720883dC451ED1Daa7694B55C9";
-//const NFT_CONTRACT_ADDRESS = "0x951706bc6900d5b718deb58f80912742172dcef3";
-//const NFT_CONTRACT_ADDRESS = "0xb7f3aAddd91a12Aa8557a962f7b40602653df92b";
-//const NFT_CONTRACT_ADDRESS = "0x4671F2E371AC50647C035DA12FCf20A4cC34E99d";
-//const NFT_CONTRACT_ADDRESS = "0xD7e275c28463dD720883dC451ED1Daa7694B55C9";
-//const NFT_CONTRACT_ADDRESS = "0x9e7d1369aA4ac325C74f8D9Fbd16c0547f349919";
-//const NFT_CONTRACT_ADDRESS = "0x98Da1D27C16AD4380715e389251c891E03103bB8";
-//const NFT_CONTRACT_ADDRESS = "0x951706bc6900D5b718dEb58f80912742172DcEf3";
-const NFT_CONTRACT_ADDRESS = "0x5b447a5FC7917978A1fabfc0B4aAB845e0a06710";
+
+const NFT_CONTRACT_ADDRESS="0x7CC83FFa5A8B8a6abAeEe4fCfF43487d4cac403a";
 
 const FACTORY_CONTRACT_ADDRESS=""
 const NETWORK="rinkeby"
-/*const MNEMONIC = process.env.MNEMONIC1;
-const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY1;
-const isInfura = !!process.env.INFURA_KEY;
-const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS1;
-const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS1;
-const OWNER_ADDRESS = process.env.OWNER_ADDRESS1;
-const NETWORK = process.env.NETWORK1;*/
+
 const NUM_CREATURES = 12;
 const NUM_LOOTBOXES = 4;
 const DEFAULT_OPTION_ID = 0;
@@ -39,19 +27,58 @@ if (!MNEMONIC || !NODE_API_KEY || !OWNER_ADDRESS || !NETWORK) {
 
 const NFT_ABI = [
   {
-    constant: false,
-    inputs: [
-      {
-        name: "_to",
-        type: "address",
-      },
-    ],
-    name: "mintTo",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
+
+  "constant": false,
+      "inputs": [
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "name": "tokenURI",
+          "type": "string"
+        }
+      ],
+      "name": "mint",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+      //"signature": "0xd3fc9864"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "tokenURI",
+          "type": "string"
+        },
+        {
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "setTokenURI",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x09a3beef"
+    },
 ];
 
 const FACTORY_ABI = [
@@ -120,19 +147,45 @@ async function main() {
       NFT_CONTRACT_ADDRESS,
       { gasLimit: "1000000" }
     );
+    console.log(NFT_ABI, "THIS IS NFT ABI");
         
     // Creatures issued directly to the owner.
-    for (var i = 0; i < NUM_CREATURES; i++) {
+   //for (var i = 0; i < NUM_CREATURES; i++) {
       const result = await nftContract.methods
-        .mint(OWNER_ADDRESS, 1234, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/")
+        .mint(OWNER_ADDRESS, 23 , "https://my-json-server.typicode.com/sani-ikram/Blockchain---RealEstate-Project/tokens/")
         .send({ from: OWNER_ADDRESS });
+
       console.log("Minted creature. Transaction: " + result.transactionHash);
+        
+      const result1 = await nftContract.methods
+        .setTokenURI("https://my-json-server.typicode.com/sani-ikram/Blockchain---RealEstate-Project/tokens/",23)
+        .call({ from: OWNER_ADDRESS });
+      
+        var metadata_url = JSON.stringify(result1);
+       console.log(`Metadata URL: ${metadata_url}`);
+       //console.log(metadata_url);
+       const metadata = await fetch(metadata_url).then(res => res.json());
+      console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`);
     }
-  } else {
-    console.error(
-      "Add NFT_CONTRACT_ADDRESS or FACTORY_CONTRACT_ADDRESS to the environment variables"
-    );
-  }
+  /*  task("token-uri", "Fetches the token metadata for the given token ID")
+  .addParam("tokenId", "The tokenID to fetch metadata for")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("NFT", hre);
+    const response = await contract.tokenURI(taskArguments.tokenId, {
+        gasLimit: 500_000,
+    });
+    
+    const metadata_url = response;
+    console.log(`Metadata URL: ${metadata_url}`);*/
+
+    //const metadata = await fetch(metadata_url).then(res => res.json());
+    //console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`);
+//});
+  //} /*else {
+    //console(
+     // "Add NFT_CONTRACT_ADDRESS or FACTORY_CONTRACT_ADDRESS to the environment variables");
+   // }*/
+    //console.log(result);
 }
 
 main();
